6928ae0e62bc56207f7b8e6adcad2b9c1d21a789
MrDummy <mrdummy@MrDummys-Mac.local>
2017-06-06 20:00:58 +0700

    Initial Commit

---
+++ Mock-Cinema/LoginViewController.swift
@@ -0,0 +1,61 @@
+//
+//  LoginViewController.swift
+//  Mock-Cinema
+//
+//  Created by Cntt35 on 6/3/17.
+//  Copyright © 2017 Huy. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import FirebaseAuth
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var txtEmail: UITextField!
+    @IBOutlet weak var txtPassword: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func btnLogin(_ sender: Any) {
+        if (txtEmail.text?.isEmpty)! || (txtPassword.text?.isEmpty)! {
+            self.displayMyAlertMessage(userMessage: "Please fill out all required fields")
+        } else {
+        
+            guard let email = txtEmail.text, let password = txtPassword.text else {
+                print("Please fill out all required fields")
+                return
+            }
+            
+            Auth.auth().signIn(withEmail: email, password: password) { (user, error) in
+                if error != nil {
+                    self.displayMyAlertMessage(userMessage: "Wrong email or password")
+                    self.txtEmail.text?.removeAll()
+                    self.txtPassword.text?.removeAll()
+                    return
+                } else {
+                    // login successful
+                    self.performSegue(withIdentifier: "goHome", sender: self)
+                }
+            }
+        }
+    }
+    
+    func displayMyAlertMessage(userMessage: String) {
+        let myAlert = UIAlertController(title: "Alert", message: userMessage, preferredStyle: UIAlertControllerStyle.alert)
+        
+        let okAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil)
+        
+        myAlert.addAction(okAction)
+        self.present(myAlert, animated: true, completion: nil)
+    }
+}
